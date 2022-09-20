6d3b80668963e0493ff523caabdcff37788a6108
Refka <refka.gmati@gmail.com>
2022-09-20 01:00:29 +0100

    reactflamantproject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import CardDetails from "./component/CardDetails";
+import NavBar from "./component/NavBar";
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
+import img4 from "./images/img4.jpg";
+import img5 from "./images/img5.jpg";
+import img6 from "./images/img6.jpg";
+import Footer from "./component/Footer";
 
-function App() {
+const App = () => {
+  const data = [
+    { title: "Robe", description: "description 1", img: img1 },
+    { title: "Chapeau", description: "description 2", img: img2 },
+    { title: "Bijoux", description: "description 3", img: img3 },
+    { title: "Sac", description: "description 4", img: img4 },
+    { title: "Maquillage", description: "description 5", img: img5 },
+    { title: "Balade en Bateaux", description: "description 6", img: img6 },
+  ];
+
+  const printTitle = (message) => {
+    alert(" this from app file" + message);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      {data.map((item, index) => {
+        return (
+          <CardDetails
+            key={index}
+            title={item.title}
+            description={item.description}
+            img={item.img}
+            clickMe={printTitle}
+          />
+        );
+      })}
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
