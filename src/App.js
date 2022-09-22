a37caf2397c1e8cde453d5e70a01631c7857c2c9
Refka <refka.gmati@gmail.com>
2022-09-22 17:26:51 +0100

    establishment routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
-import CardDetails from "./component/CardDetails";
+import React, { Component } from "react";
 import NavBar from "./component/NavBar";
-import img1 from "./images/img1.jpg";
-import img2 from "./images/img2.jpg";
-import img3 from "./images/img3.jpg";
-import img4 from "./images/img4.jpg";
-import img5 from "./images/img5.jpg";
-import img6 from "./images/img6.jpg";
 import Footer from "./component/Footer";
+import { Routes, Route } from "react-router-dom";
+import Inscription from "./component/Inscription";
+import Connexion from "./component/Connexion";
+import Home from "./component/Home";
 
-const App = () => {
-  const data = [
-    { title: "Robe", description: "description 1", img: img1 },
-    { title: "Chapeau", description: "description 2", img: img2 },
-    { title: "Bijoux", description: "description 3", img: img3 },
-    { title: "Sac", description: "description 4", img: img4 },
-    { title: "Maquillage", description: "description 5", img: img5 },
-    { title: "Balade en Bateaux", description: "description 6", img: img6 },
-  ];
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/inscription" element={<Inscription />} />
+          <Route exact path="/inscription" element={<Inscription />}></Route>
+          <Route path="/connexion" element={<Connexion />}></Route>
+        </Routes>
 
-  const printTitle = (message) => {
-    alert(" this from app file" + message);
-  };
-  return (
-    <div>
-      <NavBar />
-      {data.map((item, index) => {
-        return (
-          <CardDetails
-            key={index}
-            title={item.title}
-            description={item.description}
-            img={item.img}
-            clickMe={printTitle}
-          />
-        );
-      })}
-      <Footer />
-    </div>
-  );
-};
-
+        <Footer />
+      </div>
+    );
+  }
+}
 export default App;
